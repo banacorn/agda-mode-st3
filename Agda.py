8dd0a95f59e41221286fd070c81d342d213dee62
Ting-Yen Lai <banacorn@gmail.com>
2014-07-22 21:33:09 +0800

    agda load source

--- Agda.py
+++ Agda.py
@@ -8,5 +8,15 @@ class OnLoadCommand(sublime_plugin.EventListener):
             return
         if filename.endswith(".agda"):
             sublime.status_message("Welcome to Agda mode")
+            
+            view.set_syntax_file("Packages/Agda/NoAgda.tmLanguage")
 
+class LoadSourceCommand(sublime_plugin.TextCommand):
 
+    def run(self, edit):
+
+        filename = self.view.file_name()
+        if not filename:  # buffer has never been saved
+            return
+        if filename.endswith(".agda"):
+            self.view.set_syntax_file("Packages/Agda/Agda.tmLanguage")
