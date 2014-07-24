40a6399a0964db8e4e990677b7ff23b1e111481a
Ting-Yen Lai <banacorn@gmail.com>
2014-07-24 19:05:09 +0800

    Quit <Ctrl-c-x-q>

--- Agda.py
+++ Agda.py
@@ -1,9 +1,5 @@
-import sublime, sublime_plugin, sys, os
-
-import json
-from pprint import pprint
-
-# from subprocess import Popen, PIPE, check_output, call
+import sublime, sublime_plugin
+import os
 
 class EventCommand(sublime_plugin.EventListener):
 
@@ -12,11 +8,8 @@ class EventCommand(sublime_plugin.EventListener):
         if not filename:  # buffer has never been saved
             return
         if filename.endswith('.agda'):
-
-            print('loaded')
-
-            self.activateMenu()
-            self.deactivateSyntax(view)
+            activateMenu()
+            deactivateSyntax(view)
 
 
     def on_close(self, view):
@@ -24,40 +17,43 @@ class EventCommand(sublime_plugin.EventListener):
         if not filename:  # buffer has never been saved
             return
         if filename.endswith('.agda'):
-            self.deactivateMenu()
-
-            
-    def activateMenu(self):
-        oldPath = sublime.packages_path() + '/Agda/Menus/NoMain.sublime-menu'
-        newPath = sublime.packages_path() + '/Agda/Menus/Main.sublime-menu'
-        os.rename(oldPath, newPath)
+            deactivateMenu()
 
 
-    def deactivateMenu(self):
-        oldPath = sublime.packages_path() + '/Agda/Menus/Main.sublime-menu'
-        newPath = sublime.packages_path() + '/Agda/Menus/NoMain.sublime-menu'
-        os.rename(oldPath, newPath)
-
-    def deactivateSyntax(self, view):
-        view.set_syntax_file('Packages/Agda/NoAgda.tmLanguage')
-
-
-    # def initialize(self):
-    #     configPath = sublime.packages_path() + '/Agda/configuration.json'
-    #     config = json.load(open(configPath))
-    #     pprint(config)
-
-    #     if config['initialized'] == 'False' :
-    #         self.window.show_input_panel("Goto Line:", "", self.on_done, None, None)
-
-
-class LoadSourceCommand(sublime_plugin.TextCommand):
+class LoadCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
-
         filename = self.view.file_name()
         if not filename:  # buffer has never been saved
             return
         if filename.endswith('.agda'):
-            self.view.set_syntax_file('Packages/Agda/Agda.tmLanguage')
+            activateSyntax(self.view)
+
+class QuitCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        filename = self.view.file_name()
+        if not filename:  # buffer has never been saved
+            return
+        if filename.endswith('.agda'):
+            deactivateSyntax(self.view)
+
+
+        
+def activateMenu():
+    oldPath = sublime.packages_path() + '/Agda/Menus/NoMain.sublime-menu'
+    newPath = sublime.packages_path() + '/Agda/Menus/Main.sublime-menu'
+    os.rename(oldPath, newPath)
+
+
+def deactivateMenu():
+    oldPath = sublime.packages_path() + '/Agda/Menus/Main.sublime-menu'
+    newPath = sublime.packages_path() + '/Agda/Menus/NoMain.sublime-menu'
+    os.rename(oldPath, newPath)
+
+def activateSyntax(view):
+    view.set_syntax_file('Packages/Agda/Agda.tmLanguage')
+
+
+def deactivateSyntax(view):
+    view.set_syntax_file('Packages/Agda/NoAgda.tmLanguage')
 
